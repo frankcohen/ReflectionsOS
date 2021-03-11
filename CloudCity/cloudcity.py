644a1dd6a72713bac491ebbef7c2dbefe4689f7f
Frank Cohen <fcohen@votsh.com>
2021-03-10 20:03:25 -0800

    Update cloudcity.py
    Finished recoding features in python:
    /onefilename
    /download?file=myfile.mjpeg
    /delete?file=myfile.mjpeg
    / gets you the upload form

--- CloudCity/cloudcity.py
+++ CloudCity/cloudcity.py
@@ -1,170 +1,97 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
-'''
- File upload/download services for the Reflections devices
+# Copyright Jon Berg , turtlemeat.com
+# Modified by nikomu @ code.google.com
 
- Reflections project: A wrist watch
- Seuss Display: The watch display uses a breadboard with ESP32, OLED display, audio
- player/recorder, SD card, GPS, and accelerometer/compass
- Repository and community discussions at https://github.com/frankcohen/ReflectionsOS
-
- Licensed under GPL v3
- (c) Frank Cohen, All rights reserved. fcohen@votsh.com
- February 13, 2021
-
- Thank you Tao Huang of UniIsland for writing the code this is derived from:
- see: https://gist.github.com/UniIsland/3346170
- 'This module builds on BaseHTTPServer by implementing the standard GET
- and HEAD requests in a fairly straightforward manner.'
-
- Available features:
- http://server-ip/download?file=fileone.jpeg gets a file from the server
- http://server-ip/ browses the file upload form
- http://server-ip/delete?file=fileone.jpeg delete a file
- http://server-ip/onefilename browses a single file on the server
-
- Notes on how I build and deploy this service:
- Uses Python's SimpleHTTPServer Module
-   https://stackabuse.com/serving-files-with-pythons-simplehttpserver-module/
- Not for production use, there is no security to this service
- It's running on port 80 with no libraries.
- I threw this together quickly and would be glad for your improvements.
-
- AWS Lightsail Node.js and Mongo server
- https://docs.bitnami.com/aws/infrastructure/mean/
- https://docs.bitnami.com/aws/
- https://community.bitnami.com/
-
- Run the server:
- sudo python3 cloudcity.py
-
- stop it:
- ps -ef|grep python3
- sudo kill -9 process number
-'''
-
-__version__ = "1.0"
-__all__ = ["CloudCity"]
-__author__ = "fcohen@starlingwatch.com"
-__home_page__ = "http://starlingwatch.com/"
-
-import os
-import posixpath
-import http.server
-import urllib.request, urllib.parse, urllib.error
-import html
-import shutil
-import mimetypes
+import string,cgi,time
+from os import curdir, sep
+from http.server import BaseHTTPRequestHandler, HTTPServer, BaseHTTPRequestHandler, CGIHTTPRequestHandler
+from urllib import parse
 import re
-from io import BytesIO
-import cgi
+import os # os. path
 
-hostName = "0.0.0.0"
-serverPort = 80
+CWD = os.path.abspath('.')
+## print CWD
 
-class CloudCityServer(http.server.BaseHTTPRequestHandler):
+PORT = 8080
+UPLOAD_PAGE = 'upload.html' # must contain a valid link with address and port of the server     s
 
-    """Simple HTTP request handler with GET/HEAD/POST commands.
 
-    This serves files from the current directory and any of its
-    subdirectories.  The MIME type for files is determined by
-    calling the .guess_type() method. And can reveive file uploaded
-    by client.
 
-    The GET/HEAD/POST requests are identical except that the HEAD
-    request omits the actual contents of the file.
+# -----------------------------------------------------------------------
 
-    """
-
-    server_version = "SimpleHTTPWithUpload/" + __version__
+class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        """Serve a GET request."""
+        try:
 
-        if self.path == '/onefilename':
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+            if self.path == '/' :
+                self.send_response(200)
+                self.send_header('Content-type',	'text/html')
+                self.end_headers()
+                self.wfile.write( ( "<form ENCTYPE=\"multipart/form-data\" method=\"post\">" ).encode() )
+                self.wfile.write( ( "<input name=\"file\" type=\"file\"/>" ).encode() )
+                self.wfile.write( ( "<input type=\"submit\" value=\"upload\"/></form>\n" ).encode() )
+                return
 
-            self.wfile.write( os.listdir('/')[0] )
+            elif ( self.path.find( 'download' ) != -1):
 
-            f = self.send_head()
-            if f:
-                self.copyfile(f, self.wfile)
+                self.myparams = dict(parse.parse_qsl(parse.urlsplit(self.path).query))
+
+                self.send_response(200)
+                self.send_header('content-type', 'application/octet-stream')
+                self.send_header('content-disposition', 'attachment;filename=' + self.myparams["file"] )
+                self.send_header('content-length', os.path.getsize( self.myparams["file"] ) )
+                self.end_headers()
+
+                f = open( self.myparams[ "file" ], 'rb' )
+                self.wfile.write( f.read() )
+
+            elif ( self.path.find( 'onefilename' ) != -1):
+
+                self.send_response(200)
+                self.send_header('content-type', 'text/html')
+                self.end_headers()
+
+                self.entries = os.listdir('.')
+                self.wfile.write( self.entries[0].encode() )
+
+            elif ( self.path.find( 'delete' ) != -1):
+
+                self.myparams = dict(parse.parse_qsl(parse.urlsplit(self.path).query))
+
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write( "removed".encode() )
+
+                os.remove( self.myparams[ "file" ] )
+
+            else : # default: just send the file
+
+                filepath = self.path[1:] # remove leading '/'
+
+                f = open( os.path.join(CWD, filepath), 'rb' )
+                #note that this potentially makes every file on your computer readable by the internet
+
+                self.send_response(200)
+                self.send_header('Content-type',	'application/octet-stream')
+                self.end_headers()
+                self.wfile.write(f.read())
                 f.close()
+                return
 
-        elseif self.path == '/download':
+            return # be sure not to fall into "except:" clause ?
 
-            self.form = cgi.FieldStorage()
-            self.filename = form.getvalue('file')
+        except IOError as e :
+            # debug
+            print( e )
+            self.send_error(404,'File Not Found: %s' % self.path)
 
-            f = self.send_head()
-            if f:
-                self.copyfile(f, self.wfile)
-                f.close()
-
-        elseif self.path == 'delete':
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            try:
-                self.form = cgi.FieldStorage()
-                self.filename = form.getvalue('file')
-                os.remove( filename );
-                self.wfile.write( "<html><body>Deleted</body></html>")
-            except IOError:
-                self.wfile.write( "<html><body>Could not delete ")
-                self.wfile.write( filenname )
-                self.wfile.write( "Error " )
-                self.wfile.write( IOError )
-                self.wfile.write( "</body></html>")
-
-            f = self.send_head()
-            if f:
-                self.copyfile(f, self.wfile)
-                f.close()
-
-        else:
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write("<html><head><title>ReflectionsOS project</title></head>")
-            self.wfile.write('<body>')
-            self.wfile.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
-            self.wfile.write('<input type="file" name="filetoupload"><br>')
-            self.wfile.write('<input type="submit">')
-            self.wfile.write('<form>')
-
-            f = self.send_head()
-            if f:
-                self.copyfile(f, self.wfile)
-                f.close()
-
-    def do_HEAD(self):
-        """Serve a HEAD request."""
-        f = self.send_head()
-        if f:
-            f.close()
 
     def do_POST(self):
-        """Serve a POST request."""
-
-        if self.path == '/fileupload':
-            r, info = self.deal_post_data()
-            print((r, info, "by: ", self.client_address))
-
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.send_header("Content-Length", str(length))
-            self.end_headers()
-            if f:
-                self.copyfile(f, self.wfile)
-                f.close()
-
-    def deal_post_data(self):
         content_type = self.headers['content-type']
         if not content_type:
-            return (False, "Content-Type header doesn't contain boundary")
+            print("Content-Type header doesn't contain boundary")
         boundary = content_type.split("=")[1].encode()
         remainbytes = int(self.headers['content-length'])
         line = self.rfile.readline()
@@ -176,7 +103,7 @@ class CloudCityServer(http.server.BaseHTTPRequestHandler):
         fn = re.findall(r'Content-Disposition.*name="file"; filename="(.*)"', line.decode())
         if not fn:
             return (False, "Can't find out file name...")
-        path = self.translate_path(self.path)
+        path = "."  # self.translate_path(self.path)
         fn = os.path.join(path, fn[0])
         line = self.rfile.readline()
         remainbytes -= len(line)
@@ -198,181 +125,27 @@ class CloudCityServer(http.server.BaseHTTPRequestHandler):
                     preline = preline[0:-1]
                 out.write(preline)
                 out.close()
-                return (True, "File '%s' upload success!" % fn)
+
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write( "Uploaded".encode() )
+
+                return ("File '%s' upload success!" % fn)
             else:
                 out.write(preline)
                 preline = line
-        return (False, "Unexpect End of data.")
+        return ("Unexpect Ends of data.")
 
-    def send_head(self):
-        """Common code for GET and HEAD commands.
 
-        This sends the response code and MIME headers.
-
-        Return value is either a file object (which has to be copied
-        to the outputfile by the caller unless the command was HEAD,
-        and must be closed by the caller under all circumstances), or
-        None, in which case the caller has nothing further to do.
-
-        """
-        path = self.translate_path(self.path)
-        f = None
-        if os.path.isdir(path):
-            if not self.path.endswith('/'):
-                # redirect browser - doing basically what apache does
-                self.send_response(301)
-                self.send_header("Location", self.path + "/")
-                self.end_headers()
-                return None
-            for index in "index.html", "index.htm":
-                index = os.path.join(path, index)
-                if os.path.exists(index):
-                    path = index
-                    break
-            else:
-                return self.list_directory(path)
-        ctype = self.guess_type(path)
-        try:
-            # Always read in binary mode. Opening files in text mode may cause
-            # newline translations, making the actual size of the content
-            # transmitted *less* than the content-length!
-            f = open(path, 'rb')
-        except IOError:
-            self.send_error(404, "File not found")
-            return None
-        self.send_response(200)
-        self.send_header("Content-type", ctype)
-        fs = os.fstat(f.fileno())
-        self.send_header("Content-Length", str(fs[6]))
-        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-        self.end_headers()
-        return f
-
-    def list_directory(self, path):
-        """Helper to produce a directory listing (absent index.html).
-
-        Return value is either a file object, or None (indicating an
-        error).  In either case, the headers are sent, making the
-        interface the same as for send_head().
-
-        """
-        try:
-            list = os.listdir(path)
-        except os.error:
-            self.send_error(404, "No permission to list directory")
-            return None
-        list.sort(key=lambda a: a.lower())
-        f = BytesIO()
-        displaypath = html.escape(urllib.parse.unquote(self.path))
-        f.write(b'<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-        f.write(("<html>\n<title>Directory listing for %s</title>\n" % displaypath).encode())
-        f.write(("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath).encode())
-        f.write(b"<hr>\n")
-        f.write(b"<form ENCTYPE=\"multipart/form-data\" method=\"post\">")
-        f.write(b"<input name=\"file\" type=\"file\"/>")
-        f.write(b"<input type=\"submit\" value=\"upload\"/></form>\n")
-        f.write(b"<hr>\n<ul>\n")
-        for name in list:
-            fullname = os.path.join(path, name)
-            displayname = linkname = name
-            # Append / for directories or @ for symbolic links
-            if os.path.isdir(fullname):
-                displayname = name + "/"
-                linkname = name + "/"
-            if os.path.islink(fullname):
-                displayname = name + "@"
-                # Note: a link to a directory displays with @ and links with /
-            f.write(('<li><a href="%s">%s</a>\n'
-                    % (urllib.parse.quote(linkname), html.escape(displayname))).encode())
-        f.write(b"</ul>\n<hr>\n</body>\n</html>\n")
-        length = f.tell()
-        f.seek(0)
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(length))
-        self.end_headers()
-        return f
-
-    def translate_path(self, path):
-        """Translate a /-separated PATH to the local filename syntax.
-
-        Components that mean special things to the local file system
-        (e.g. drive or directory names) are ignored.  (XXX They should
-        probably be diagnosed.)
-
-        """
-        # abandon query parameters
-        path = path.split('?',1)[0]
-        path = path.split('#',1)[0]
-        path = posixpath.normpath(urllib.parse.unquote(path))
-        words = path.split('/')
-        words = [_f for _f in words if _f]
-        path = os.getcwd()
-        for word in words:
-            drive, word = os.path.splitdrive(word)
-            head, word = os.path.split(word)
-            if word in (os.curdir, os.pardir): continue
-            path = os.path.join(path, word)
-        return path
-
-    def copyfile(self, source, outputfile):
-        """Copy all data between two file objects.
-
-        The SOURCE argument is a file object open for reading
-        (or anything with a read() method) and the DESTINATION
-        argument is a file object open for writing (or
-        anything with a write() method).
-
-        The only reason for overriding this would be to change
-        the block size or perhaps to replace newlines by CRLF
-        -- note however that this the default server uses this
-        to copy binary data as well.
-
-        """
-        shutil.copyfileobj(source, outputfile)
-
-    def guess_type(self, path):
-        """Guess the type of a file.
-
-        Argument is a PATH (a filename).
-
-        Return value is a string of the form type/subtype,
-        usable for a MIME Content-type header.
-
-        The default implementation looks the file's extension
-        up in the table self.extensions_map, using application/octet-stream
-        as a default; however it would be permissible (if
-        slow) to look inside the data to make a better guess.
-
-        """
-
-        base, ext = posixpath.splitext(path)
-        if ext in self.extensions_map:
-            return self.extensions_map[ext]
-        ext = ext.lower()
-        if ext in self.extensions_map:
-            return self.extensions_map[ext]
-        else:
-            return self.extensions_map['']
-
-    if not mimetypes.inited:
-        mimetypes.init() # try to read system mime.types
-    extensions_map = mimetypes.types_map.copy()
-    extensions_map.update({
-        '': 'application/octet-stream', # Default
-        '.py': 'text/plain',
-        '.c': 'text/plain',
-        '.h': 'text/plain',
-        })
-
-if __name__ == "__main__":
-    cloudcity = CloudCityServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+def main():
 
     try:
-        cloudcity.serve_forever()
+        server = HTTPServer(('127.0.0.1', PORT), MyHandler)
+        print ('started httpserver...')
+        server.serve_forever()
     except KeyboardInterrupt:
-        pass
+        print ('^C received, shutting down server')
+        server.socket.close()
 
-    cloudcity.server_close()
-    print("Server stopped.")
+if __name__ == '__main__':
+    main()
