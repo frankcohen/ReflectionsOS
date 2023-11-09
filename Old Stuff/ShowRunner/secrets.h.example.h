93dee76511ffcae1061030299285425b6b3f41ff
Frank Cohen <fcohen@votsh.com>
2023-11-08 19:46:18 -0800

    Pruning old stuff

--- Old Stuff/ShowRunner/secrets.h.example.h
+++
@@ -1,6 +0,0 @@
-// Defines IDs and Passwords
-// Github repository .gitignore file prevents this from being added
-// For examples of this file see secrets.h.example
-
-// #define SECRET_SSID "Frank"
-// #define SECRET_PASSWORD ""
